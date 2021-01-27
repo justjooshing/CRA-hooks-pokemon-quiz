4656642d3ec4ffdffa4f22d25ddb9968dc6c1c31
Joshua Mertens <joshua.l.mertens@gmail.com>
2021-01-27 09:32:05 +0000

    add scroll to top on load for instructions page

--- client/src/components/instructions-page/InstructionsPage.jsx
+++ client/src/components/instructions-page/InstructionsPage.jsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
 import Instructions from "./instructions/Instructions";
 import ContinueButton from "./continue-button/ContinueButton";
 import ModeIndicator from "../indicators/ModeIndicator";
 
 export default function InstructionsPage() {
+  //onload, scroll to top
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  });
+
   return (
     <>
       <ModeIndicator />
