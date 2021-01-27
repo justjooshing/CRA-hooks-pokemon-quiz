0150982dc90b18c641744df2bcc16ff65123e01b
Joshua Mertens <joshua.l.mertens@gmail.com>
2021-01-27 12:13:18 +0000

    add semantics to loading screen and center it

--- client/src/components/quiz/LoadingScreen.jsx
+++ client/src/components/quiz/LoadingScreen.jsx
@@ -4,13 +4,13 @@ import pokeball from "../../images/quiz/pokeball.png";
 
 export default function LoadingScreen() {
   return (
-    <div className="loading_pokeball_wrapper">
+    <main className="loading_pokeball_wrapper">
       <img src={pokeball} alt="pokeball" className="loading_pokeball" />
       <h1 className="loading_text">
         Loading<span className="ellipsis_dot_one">.</span>
         <span className="ellipsis_dot_two">.</span>
         <span className="ellipsis_dot_three">.</span>
       </h1>
-    </div>
+    </main>
   );
 }
