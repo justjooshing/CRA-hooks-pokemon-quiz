46da3236a4508eccd97796001623944beb89f9c6
Joshua Mertens <joshua.l.mertens@gmail.com>
2021-01-28 17:36:37 +0000

    change logic to use mongo rather than json file for highscores

--- server.js
+++ server.js
@@ -1,39 +1,34 @@
 const express = require("express");
 const path = require("path");
-const { nanoid } = require("nanoid");
-
-let infiniteModeHighScores = require("./highScores/infiniteModeHighScores.json");
-const { writeDataToFile } = require("./utils");
+const sendHighScores = require("./utils");
+const HighScore = require("./utils/scoreSchema");
+//db not read but connection still loaded
+const db = require("./dbConnect");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.get("/api/names", (req, res) => {
-  res.json({ scores: infiniteModeHighScores });
+  sendHighScores(res);
 });
 
 app.post("/api/names", (req, res) => {
   const { score, name } = req.body;
-  if (score && name) {
-    const id = nanoid(10);
-    infiniteModeHighScores.push({ name, score, id });
-  }
-  //Order them by score highest to lowest
-  infiniteModeHighScores.sort((a, b) =>
-    a.score < b.score ? 1 : b.score < a.score ? -1 : 0
-  );
-  //keep only highest three
-  infiniteModeHighScores = infiniteModeHighScores.slice(0, 3);
-  writeDataToFile(
-    "./highScores/infiniteModeHighScores.json",
-    infiniteModeHighScores
-  );
-  res.status(200).send({ infiniteModeHighScores });
+  const newHighScore = new HighScore({
+    name,
+    score,
+  });
+  newHighScore.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      sendHighScores(res);
+    }
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
