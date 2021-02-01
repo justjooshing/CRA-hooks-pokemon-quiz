58b4fe8ac1cfd889582a4a0314f7c10578827a92
Joshua Mertens <joshua.l.mertens@gmail.com>
2021-02-01 18:11:29 +0000

    fix tests path

---
+++ client/src/functions/__tests__/quizFunctions.spec.js
@@ -0,0 +1,22 @@
+import { testFn, shuffle, grabAllPokemon } from "../quizFunctions";
+
+describe("shuffle function", () => {
+  // it("doesn't return an unshuffled array", () => {
+  //   const unshuffled = [1, 2, 3, 4, 5];
+  //   const actual = shuffle([1, 2, 3, 4, 5]);
+  //   expect(actual).not.toBe(unshuffled);
+  // });
+  // it("errors on empty array", () => {
+  //   const error = "Shuffle array is empty";
+  //   const actual = testFn([]);
+  //   expect(actual).toThrowError(error);
+  // });
+});
+
+describe("grab all pokemon", () => {
+  it("returns an array of n pokemon names", async () => {
+    const actual = await grabAllPokemon(155);
+    const expected = 155;
+    expect(actual.length).toEqual(expected);
+  });
+});
