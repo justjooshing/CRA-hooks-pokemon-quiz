b3926a5b6ad8646ece800971f19be323d931b444
Joshua Mertens <joshua.l.mertens@gmail.com>
2021-02-02 16:06:28 +0000

    add unit tests and throw errors for quiz functions

--- client/src/functions/__tests__/quizFunctions.spec.js
+++ client/src/functions/__tests__/quizFunctions.spec.js
@@ -1,22 +1,213 @@
-import { testFn, shuffle, grabAllPokemon } from "../quizFunctions";
+import {
+  shuffle,
+  grabAllPokemon,
+  generateQuestionTopics,
+  generatePokemonQuestions,
+  generatePossibleAnswers,
+  checkTempAnswer,
+} from "../quizFunctions";
 
 describe("shuffle function", () => {
-  // it("doesn't return an unshuffled array", () => {
-  //   const unshuffled = [1, 2, 3, 4, 5];
-  //   const actual = shuffle([1, 2, 3, 4, 5]);
-  //   expect(actual).not.toBe(unshuffled);
-  // });
-  // it("errors on empty array", () => {
-  //   const error = "Shuffle array is empty";
-  //   const actual = testFn([]);
-  //   expect(actual).toThrowError(error);
-  // });
+  it("doesn't return an unshuffled array", () => {
+    const unshuffled = [1, 2, 3, 4, 5];
+    const actual = shuffle([1, 2, 3, 4, 5]);
+    expect(actual).not.toEqual(unshuffled);
+  });
+  it("errors on empty array", () => {
+    const error = "Shuffle array is empty or not an array";
+    const actual = () => shuffle([]);
+    expect(actual).toThrowError(error);
+  });
+  it("errors on object argument", () => {
+    const error = "Shuffle array is empty or not an array";
+    const actual = () => shuffle({ test: 1, bar: "foo" });
+    expect(actual).toThrowError(error);
+  });
+  it("errors on string argument", () => {
+    const error = "Shuffle array is empty or not an array";
+    const actual = () => shuffle("tomato soup");
+    expect(actual).toThrowError(error);
+  });
+  it("errors on no argument", () => {
+    const error = "Shuffle array is empty or not an array";
+    const actual = () => shuffle();
+    expect(actual).toThrowError(error);
+  });
 });
 
 describe("grab all pokemon", () => {
   it("returns an array of n pokemon names", async () => {
+    expect.assertions(1);
     const actual = await grabAllPokemon(155);
     const expected = 155;
     expect(actual.length).toEqual(expected);
+  });
+  it("errors when n is too high", async () => {
+    expect.assertions(1);
+    const actual = await grabAllPokemon(1550);
+    const error =
+      "input number more than total number of pokemon (currently 898)";
+    await expect(actual).toEqual(error);
+  });
+  it("errors when n is less than gen 1 number", async () => {
+    expect.assertions(1);
+    const actual = await grabAllPokemon(14);
+    const error = "input number less than Gen 1 number of pokemon (151)";
+    await expect(actual).toEqual(error);
+  });
+  it("errors when called without an argument", async () => {
+    expect.assertions(1);
+    const actual = await grabAllPokemon();
+    const error = "total number of pokemon to grab from API not given";
+    await expect(actual).toEqual(error);
+  });
+  it("errors when called with an object", async () => {
+    expect.assertions(2);
+    const error = "total number of pokemon given as object";
+    const objectActual = await grabAllPokemon({ b: 1, c: "dog" });
+    await expect(objectActual).toEqual(error);
+    const arrayActual = await grabAllPokemon([1, 2, 3, "tuna", "banana", true]);
+    await expect(arrayActual).toEqual(error);
+  });
+});
+
+describe("generates type/name for question topics", () => {
+  it("returns an array with length equal to questions given", () => {
+    //should I be testing that the objects contain something?
+    //This should be being picked up by generatePokemonQuestions
+    const pokemonQuestions = [{}, {}, {}, {}, {}, {}];
+    const expected = 6;
+    const actual = generateQuestionTopics(pokemonQuestions);
+    expect(actual.length).toEqual(expected);
+  });
+  it("extends an array when pokemon questions are increased", () => {
+    const pokemonQuestions = [{}, {}, {}, {}, {}, {}, {}, {}];
+    const currentTopics = ["type", "name", "type", "type", "name", "type"];
+    const expected = 8;
+    const actual = generateQuestionTopics(pokemonQuestions, currentTopics);
+    expect(actual.length).toEqual(expected);
+  });
+  it("errors when argument is empty or not an array", () => {
+    const error =
+      "Cannot generate topics as pokemon questions is empty or not an array";
+    const noArg = () => generateQuestionTopics();
+    expect(noArg).toThrow(error);
+    const obj = () => generateQuestionTopics({ foo: "bar" });
+    expect(obj).toThrow(error);
+    const str = () => generateQuestionTopics("potato farmer");
+    expect(str).toThrow(error);
+  });
+});
+
+describe("generating each individual pokemon question dataset (name, image, type)", () => {
+  it("generates 10 initially", async () => {
+    const totalNumberOfPokemon = 151;
+    const numberOfRounds = 10;
+    const actual = await generatePokemonQuestions(
+      numberOfRounds,
+      totalNumberOfPokemon
+    );
+    await expect(Object.keys(actual).length).toEqual(10);
+  });
+  it("generates extra questions equal to number of rounds", async () => {
+    const totalNumberOfPokemon = 151;
+    const numberOfRounds = 15;
+    const currentPokemonQuestions = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
+    const actual = await generatePokemonQuestions(
+      numberOfRounds,
+      totalNumberOfPokemon,
+      currentPokemonQuestions
+    );
+    await expect(Object.keys(actual).length).toEqual(15);
+  });
+  it("errors when n is too high", async () => {
+    expect.assertions(1);
+    const totalNumberOfPokemon = 1550;
+    const numberOfRounds = 10;
+    const actual = await generatePokemonQuestions(
+      numberOfRounds,
+      totalNumberOfPokemon
+    );
+    const error =
+      "input number more than total number of pokemon (currently 898)";
+    await expect(actual).toEqual(error);
+  });
+  it("errors when n is less than gen 1 number", async () => {
+    expect.assertions(1);
+    const totalNumberOfPokemon = 20;
+    const numberOfRounds = 10;
+    const actual = await generatePokemonQuestions(
+      numberOfRounds,
+      totalNumberOfPokemon
+    );
+    const error = "input number less than Gen 1 number of pokemon (151)";
+    await expect(actual).toEqual(error);
+  });
+});
+
+describe("generates multiple choice sets in easy mode", () => {
+  it("returns a set of possible answers for each pokemon", async () => {
+    const answers = [
+      { name: "test", type: "beetle" },
+      { name: "john", type: "frog" },
+      { name: "greg", type: "bathtub" },
+    ];
+    const topics = ["type", "name", "type"];
+    const totalNumber = 151;
+    const actual = await generatePossibleAnswers(answers, topics, totalNumber);
+    const expected = 3;
+    await expect(actual.length).toEqual(expected);
+  });
+  it("errors when n is too high", async () => {
+    expect.assertions(1);
+    const answers = [
+      { name: "test", type: "beetle" },
+      { name: "john", type: "frog" },
+      { name: "greg", type: "bathtub" },
+    ];
+    const totalNumber = 1500;
+    const topics = ["type", "name", "type"];
+    const actual = await generatePossibleAnswers(answers, topics, totalNumber);
+    const error =
+      "input number more than total number of pokemon (currently 898)";
+    await expect(actual).toEqual(error);
+  });
+  it("errors when n is less than gen 1 number", async () => {
+    expect.assertions(1);
+    const answers = [
+      { name: "test", type: "beetle" },
+      { name: "john", type: "frog" },
+      { name: "greg", type: "bathtub" },
+    ];
+    const totalNumber = 15;
+    const topics = ["type", "name", "type"];
+    const actual = await generatePossibleAnswers(answers, topics, totalNumber);
+    const error = "input number less than Gen 1 number of pokemon (151)";
+    await expect(actual).toEqual(error);
+  });
+});
+
+describe("check for minor discrepancies in submitted answer", () => {
+  it("removes punctuation from input", () => {
+    let tempFarfetchd = "farfetch'd";
+    const correctFarfetchd = "farfetchd";
+    expect(checkTempAnswer(tempFarfetchd, correctFarfetchd)).toEqual({
+      holdTempAnswer: correctFarfetchd,
+      valid: true,
+    });
+    let tempMime = "mr mime.";
+    const correctMime = "mr mime";
+    expect(checkTempAnswer(tempMime, correctMime)).toEqual({
+      holdTempAnswer: correctMime,
+      valid: true,
+    });
+  });
+  it("reverses and checks answers with /", () => {
+    const tempAnswer = "fire/horse";
+    const correctAnswer = "horse/fire";
+    expect(checkTempAnswer(tempAnswer, correctAnswer)).toEqual({
+      holdTempAnswer: correctAnswer,
+      valid: true,
+    });
   });
 });
--- client/src/functions/quizFunctions.js
+++ client/src/functions/quizFunctions.js
@@ -1,13 +1,8 @@
 import allTypes from "../components/pokemonTypes";
 
-export const testFn = (array) => {
-  throw new Error("Shuffle array is empty");
-};
-
 export const shuffle = (array) => {
   if (!Array.isArray(array) || !array.length) {
-    console.log("empty");
-    throw new Error("Shuffle array is empty");
+    throw new Error("Shuffle array is empty or not an array");
   }
 
   let currentIndex = array.length,
@@ -30,6 +25,20 @@ export const shuffle = (array) => {
 
 export const grabAllPokemon = async (totalNumberOfPokemon) => {
   try {
+    if (typeof totalNumberOfPokemon !== "number") {
+      switch (typeof totalNumberOfPokemon) {
+        case "object":
+          throw new Error("total number of pokemon given as object");
+        default:
+          throw new Error("total number of pokemon to grab from API not given");
+      }
+    } else if (totalNumberOfPokemon > 898) {
+      throw new Error(
+        "input number more than total number of pokemon (currently 898)"
+      );
+    } else if (totalNumberOfPokemon < 151) {
+      throw new Error("input number less than Gen 1 number of pokemon (151)");
+    }
     const allPokemonFetch = await fetch(
       `https://pokeapi.co/api/v2/pokemon/?limit=${totalNumberOfPokemon}`
     );
@@ -54,11 +63,16 @@ export const grabAllPokemon = async (totalNumberOfPokemon) => {
     });
     return allPokemonNames;
   } catch (error) {
-    console.log(error.message);
+    return error.message;
   }
 };
 
 export const generateQuestionTopics = (pokemonQuestions, answerTopics = []) => {
+  if (!Array.isArray(pokemonQuestions) || !pokemonQuestions.length) {
+    throw new Error(
+      "Cannot generate topics as pokemon questions is empty or not an array"
+    );
+  }
   while (answerTopics.length < pokemonQuestions.length) {
     const questionTopic = ["name", "type"];
     const chosenTopic = questionTopic[Math.floor(Math.random() * 2)];
@@ -68,18 +82,25 @@ export const generateQuestionTopics = (pokemonQuestions, answerTopics = []) => {
 };
 
 export const generatePokemonQuestions = async (
-  numberOfPokemon,
+  numberOfRounds,
   totalNumberOfPokemon,
   questionSet = []
 ) => {
-  //grab n (10) number of pokemon from the API
-  while (questionSet.length < numberOfPokemon) {
-    const number = Math.floor(Math.random() * totalNumberOfPokemon) + 1;
-    const pokemon = {};
-    const nameUrl = `https://pokeapi.co/api/v2/pokemon/${number}`;
-    const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${number}.png`;
+  try {
+    if (totalNumberOfPokemon > 898) {
+      throw new Error(
+        "input number more than total number of pokemon (currently 898)"
+      );
+    } else if (totalNumberOfPokemon < 151) {
+      throw new Error("input number less than Gen 1 number of pokemon (151)");
+    }
+    //grab n (10) number of pokemon from the API
+    while (questionSet.length < numberOfRounds) {
+      const number = Math.floor(Math.random() * totalNumberOfPokemon) + 1;
+      const pokemon = {};
+      const nameUrl = `https://pokeapi.co/api/v2/pokemon/${number}`;
+      const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${number}.png`;
 
-    try {
       const singlePokemonResponse = await fetch(nameUrl);
       if (!singlePokemonResponse.ok) {
         const json = await singlePokemonResponse.json();
@@ -90,14 +111,79 @@ export const generatePokemonQuestions = async (
       //Grab and assign name
       pokemon.name = singlePokemonJSON.name;
 
-      //remove the female and male tags from the nidoran names
+      //remove hyphens and tags from pokemon names
       if (pokemon.name.includes("nidoran")) {
         pokemon.name = "nidoran";
-      }
-
-      //remove hypen from mr mime
-      if (pokemon.name === "mr-mime") {
+      } else if (pokemon.name === "mr-mime") {
         pokemon.name = "mr mime";
+      } else if (pokemon.name === "mime-jr") {
+        pokemon.name = "mime jr";
+      } else if (pokemon.name === "deoxys-normal") {
+        pokemon.name = "deoxys";
+      } else if (pokemon.name === "wormadam-plant") {
+        pokemon.name = "wormadam";
+      } else if (pokemon.name.includes("basculin")) {
+        pokemon.name = "basculin";
+      } else if (pokemon.name === "porygon-z") {
+        pokemon.name = "porygon z";
+      } else if (pokemon.name === "giratina-altered") {
+        pokemon.name = "giratina";
+      } else if (pokemon.name === "shaymin-altered") {
+        pokemon.name = "shaymin";
+      } else if (pokemon.name === "darmanitan-standard") {
+        pokemon.name = "darmanitan";
+      } else if (pokemon.name === "tornadus-incarnate") {
+        pokemon.name = "tornadus";
+      } else if (pokemon.name === "thundurus-incarnate") {
+        pokemon.name = "thundurus";
+      } else if (pokemon.name === "landorus-incarnate") {
+        pokemon.name = "landorus";
+      } else if (pokemon.name === "keldeo-ordinary") {
+        pokemon.name = "keldeo";
+      } else if (pokemon.name === "meloetta-aria") {
+        pokemon.name = "meloetta";
+      } else if (pokemon.name === "meowstic-male") {
+        pokemon.name = "meowstic";
+      } else if (pokemon.name === "aegislash-shield") {
+        pokemon.name = "aegislash";
+      } else if (pokemon.name === "pumpkaboo-average") {
+        pokemon.name = "pumpkaboo";
+      } else if (pokemon.name === "gourgeist-average") {
+        pokemon.name = "gourgeist";
+      } else if (pokemon.name === "oricorio-baile") {
+        pokemon.name = "oricorio";
+      } else if (pokemon.name === "lycanroc-midday") {
+        pokemon.name = "lycanroc";
+      } else if (pokemon.name === "wishiwashi-solo") {
+        pokemon.name = "wishiwashi";
+      } else if (pokemon.name === "minior-red-meteor") {
+        pokemon.name = "minior";
+      } else if (pokemon.name === "mimikyu-disguised") {
+        pokemon.name = "mimikyu";
+      } else if (pokemon.name === "mimikyu-disguised") {
+        pokemon.name = "mimikyu";
+      } else if (pokemon.name === "tapu-lele") {
+        pokemon.name = "tapu lele";
+      } else if (pokemon.name === "tapu-koko") {
+        pokemon.name = "tapu koko";
+      } else if (pokemon.name === "tapu-bulu") {
+        pokemon.name = "tapu bulu";
+      } else if (pokemon.name === "tapu-fini") {
+        pokemon.name = "tapu fini";
+      } else if (pokemon.name === "toxtricity-amped") {
+        pokemon.name = "toxtricity";
+      } else if (pokemon.name === "mr-rime") {
+        pokemon.name = "mr rime";
+      } else if (pokemon.name === "eiscue-ice") {
+        pokemon.name = "eiscue";
+      } else if (pokemon.name === "indeedee-male") {
+        pokemon.name = "indeedee";
+      } else if (pokemon.name === "zacian-hero") {
+        pokemon.name = "zacian";
+      } else if (pokemon.name === "zamazenta-hero") {
+        pokemon.name = "zamazenta";
+      } else if (pokemon.name === "urshifu-single-strike") {
+        pokemon.name = "urshifu";
       }
 
       //Grab, assign and join type
@@ -116,12 +202,11 @@ export const generatePokemonQuestions = async (
       ) {
         questionSet.push(pokemon);
       }
-    } catch (error) {
-      console.log(error.message);
-      return;
     }
+    return questionSet;
+  } catch (error) {
+    return error.message;
   }
-  return questionSet;
 };
 
 export const capitaliseFirstLetter = (string) => {
@@ -134,62 +219,82 @@ export const generatePossibleAnswers = async (
   totalNumberOfPokemon
 ) => {
   try {
-    const pokemonNames = await grabAllPokemon(totalNumberOfPokemon);
-    if (answers) {
-      const answerSets = [];
-
-      //Generate array of answers
-      const generateAnswers = (selectedPokemon, topic) => {
-        const possibleAnswers = [];
-
-        //Add correct type answer
-        possibleAnswers.push(selectedPokemon[topic]);
-
-        //Randomly choose 3 of the wrong type answers
-        while (possibleAnswers.length < 4) {
-          const numberOfOptions =
-            topic === "type" ? allTypes.length : pokemonNames.length;
-          const options = topic === "type" ? allTypes : pokemonNames;
-          const num = Math.floor(Math.random() * numberOfOptions);
-          if (!possibleAnswers.includes(options[num])) {
-            possibleAnswers.push(options[num]);
-          }
-        }
-        answerSets.push(possibleAnswers);
-      };
-
-      answers.forEach((selectedPokemon, index) => {
-        generateAnswers(selectedPokemon, topics[index]);
-      });
-
-      answerSets.forEach((roundOptions) => shuffle(roundOptions));
-      return answerSets;
-    } else {
+    if (!answers) {
       throw new Error(
         "No pokemon answers provided to build multiple choice questions from"
       );
+    } else if (totalNumberOfPokemon > 898) {
+      throw new Error(
+        "input number more than total number of pokemon (currently 898)"
+      );
+    } else if (totalNumberOfPokemon < 151) {
+      throw new Error("input number less than Gen 1 number of pokemon (151)");
     }
+    const pokemonNames = await grabAllPokemon(totalNumberOfPokemon);
+    const answerSets = [];
+
+    //Generate array of answers
+    const generateAnswers = (selectedPokemon, topic) => {
+      const possibleAnswers = [];
+
+      //Add correct type answer
+      possibleAnswers.push(selectedPokemon[topic]);
+
+      //Randomly choose 3 of the wrong type answers
+      while (possibleAnswers.length < 4) {
+        //I would like to change something here to limit the questions to that
+        //generations possible answers for type, because dragon/dark doesn't make sense
+        //as an answer option in gen 1
+        //I would need a map for pokemonNames.length = gen = types < x
+        const numberOfOptions =
+          topic === "type" ? allTypes.length : pokemonNames.length;
+        const options = topic === "type" ? allTypes : pokemonNames;
+        const num = Math.floor(Math.random() * numberOfOptions);
+        if (!possibleAnswers.includes(options[num])) {
+          possibleAnswers.push(options[num]);
+        }
+      }
+      answerSets.push(possibleAnswers);
+    };
+
+    answers.forEach((selectedPokemon, index) => {
+      generateAnswers(selectedPokemon, topics[index]);
+    });
+
+    answerSets.forEach((roundOptions) => shuffle(roundOptions));
+    return answerSets;
   } catch (error) {
-    console.log(error.message);
-    return;
+    return error.message;
   }
 };
 
 export const checkTempAnswer = (holdTempAnswer, correctAnswer) => {
+  //check for small errors and remove punctuation
   if (holdTempAnswer === "farfetch'd") {
     holdTempAnswer = "farfetchd";
-  } else if (holdTempAnswer === "Mr Mime.") {
-    holdTempAnswer = "Mr Mime";
+  } else if (holdTempAnswer === "mr mime.") {
+    holdTempAnswer = "mr mime";
+  } else if (holdTempAnswer === "mime jr.") {
+    holdTempAnswer = "mime jr";
+  } else if (holdTempAnswer === "porygon-z") {
+    holdTempAnswer = "porygon z";
+  } else if (holdTempAnswer === "mr rime.") {
+    holdTempAnswer = "mr rime";
+  } else if (holdTempAnswer === "porygon 2") {
+    holdTempAnswer = "porygon2";
   }
-  if (holdTempAnswer === correctAnswer) {
-    return { holdTempAnswer, valid: true };
-  } else if (holdTempAnswer.includes("/")) {
-    holdTempAnswer = holdTempAnswer.split("/").reverse().join("/");
-    if (holdTempAnswer === correctAnswer) {
-      return { holdTempAnswer, valid: true };
-    } else {
-      return { holdTempAnswer, valid: false };
-    }
+
+  //check for reverse-typed types
+  let reversedTempAnswer;
+  if (holdTempAnswer.includes("/")) {
+    reversedTempAnswer = holdTempAnswer.split("/").reverse().join("/");
+  }
+
+  if (
+    holdTempAnswer === correctAnswer ||
+    reversedTempAnswer === correctAnswer
+  ) {
+    return { holdTempAnswer: correctAnswer, valid: true };
   } else {
     return { holdTempAnswer, valid: false };
   }
